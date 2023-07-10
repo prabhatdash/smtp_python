2b11750f1fdf5186c94d04ab85d155e5f6223df2
Prabhat Das <prabhatdash0376@gmail.com>
2023-07-10 15:02:51 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
 s.login('system.gen.noreply@gmail.com','apsj#12345678')
-message="Hello Testing SMTP from Python"
+message="\nHello Testing SMTP from Python"
 s.sendmail("noreply@apsjorhat.org","prabhat@apsjorhat.org",message)
 s.quit()
